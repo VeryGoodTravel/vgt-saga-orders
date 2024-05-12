599851ba978ce5c5a98b98daedce0cc6bbe49067
Kamil Banach <banachkamil@outlook.com>
2024-05-13 01:09:09 +0200

    additional descriptions

--- Models/SagaDbContext.cs
+++ Models/SagaDbContext.cs
@@ -40,29 +40,58 @@ public class Transaction()
     public Guid TransactionId { get; set; }
     
     /// <summary>
-    /// Id of the offer as specified by the backend (verifies if another transaction for this 
+    /// ID of the offer as specified by the backend
     /// </summary>
     public int OfferId { get; set; }
     
-    public SagaState State { get; set; }
-    
+    /// <summary>
+    /// Date to book the hotel from (also the date of the flight)
+    /// </summary>
     public DateTime BookFrom { get; set; }
     
+    /// <summary>
+    /// Date to book the hotel to (also the date of the return flight)
+    /// </summary>
     public DateTime BookTo { get; set; }
     
+    /// <summary>
+    /// City of the airport the clients are going to fly off
+    /// </summary>
     public string TripFrom { get; set; }
     
+    /// <summary>
+    /// Hotel specified in the offer
+    /// </summary>
     public string HotelName { get; set; }
     
+    /// <summary>
+    /// Room type selected
+    /// </summary>
     public string RoomType { get; set; }
     
+    /// <summary>
+    /// How many adults in the booking request
+    /// </summary>
     public int AdultCount { get; set; }
     
+    /// <summary>
+    /// How many children under 18yo in the booking request
+    /// </summary>
     public int OldChildren { get; set; }
     
+    /// <summary>
+    /// How many children under 10yo in the booking request
+    /// </summary>
     public int MidChildren { get; set; }
     
+    /// <summary>
+    /// How many children under 3yo in the booking request
+    /// </summary>
     public int LesserChildren { get; set; }
+    
+    // saga data --------------------------------
+    
+    public SagaState State { get; set; }
     
     public bool? TempBookHotel { get; set; }
     
