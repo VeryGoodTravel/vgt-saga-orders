e196df2a18df55d85c63a70f3ace97a6d34a918c
Kamil Banach <banachkamil@outlook.com>
2024-05-21 19:15:27 +0200

    Rabbit fix

--- OrderService/OrderQueueHandler.cs
+++ OrderService/OrderQueueHandler.cs
@@ -61,16 +61,19 @@ public class OrderQueueHandler : IDisposable
     {
         _logger = log;
         _logger.Debug("{p}Initializing RabbitMq connections", LoggerPrefix);
-        try
+        while (_connection is not { IsOpen: true })
         {
-            _factory = GetConnectionFactoryFromConfig(config);
-            _connection = _factory.CreateConnection();
-        }
-        catch (BrokerUnreachableException e)
-        {
-            _logger.Error("{p}Couldn't connect to the RabbitMq server. Check connection string and/or connection {e}",
-                LoggerPrefix, e);
-            throw;
+            try
+            {
+                _factory = GetConnectionFactoryFromConfig(config);
+                _connection = _factory.CreateConnection();
+            }
+            catch (BrokerUnreachableException e)
+            {
+                _logger.Error(
+                    "{p}Couldn't connect to the RabbitMq server. Check connection string and/or connection {e}",
+                    LoggerPrefix, e);
+            }
         }
 
         _logger.Debug("{p}Connected to the RabbitMq server", LoggerPrefix);
